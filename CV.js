cc3f5c8283dbcf3256a1404351f4049c91ae0e6f
Giraltdereve <celine.rouaire@gmail.com>
2023-04-22 17:24:56 +0200

    modification du responsive

--- CV.js
+++ CV.js
@@ -37,35 +37,3 @@ onglets.forEach((onglet) => {
     }
   });
 });
-
-// ******* LE SWITCH (utile pour le dark mode)*******
-
-const theme = document.querySelectorAll(".theme");
-// on veut tous les éléments de la class theme
-
-// pour determiner quel théme à été cliqué
-theme.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    console.log(event.target.id);
-    document.body.classList.remove("whiteTheme", "darkTheme");
-    // il faut qu'on demande à ce que les thémes soient enlever afin de switcher à l'infini entre les thémes : mettre cette lmigne en commentaire si pas compris et test
-    switch (event.target.id) {
-      case "dark":
-        document.body.classList.add("darkTheme");
-        break;
-      // IMPORTANT de mettre "break" afin que l'action se termine ici et ne lise pas la suite pour appliquer les autres thémes
-      case "white":
-        document.body.classList.add("whiteTheme");
-        break;
-      default:
-        null; // dans le cas où les thémes ne fonctionnent pas
-    }
-    // traduction du switch : suivant le event.target.id, je veux que tu fasse la chose suivant
-  });
-  // on créé un même événement sur les trois thémes contenus dans "théme" via la fonction fléchée
-});
-
-// Pour créer le même évenement sur plusieurs éléments :
-// 1) mettre la même class sur chaque et ensuite un id différent ou autre class
-// 2) nommer la class unique en constante
-// 3) attribuer la même action "for each" (ligne 68)
